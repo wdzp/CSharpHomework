66be7b466dd4b641130ef57de78efe64029e3061
wdzp <43175481+wdzp@users.noreply.github.com>
2018-10-08 18:13:53 +0800

    HomeWork4

---
+++ HomeWork4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Time : EventArgs
+    {
+        public int h;
+        public int m;
+        public int s;
+    }
+    public delegate void TimeNow(object sender, Time e);
+    public class Ti
+    {
+        public void DoSet()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            bool q = true;
+            while (q)
+            {
+                Time args = new Time();
+                args.h = DateTime.Now.Hour;
+                args.m = DateTime.Now.Minute;
+                args.s = DateTime.Now.Second;
+                if (args.h == a & args.m == b&args.s==c)
+                    q = false;
+            }
+        }
+    }
+    public class TimeSet
+    {
+        static void Main()
+        {
+            Ti ti = new Ti();
+            ti.DoSet();
+        }
+    }
+}
---
+++ HomeWork4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    public class OrderService
+    {
+        public class Order : OrderService
+        {
+            public string num;
+            public string name;
+            public string people;
+            public class OrderDetails : Order
+            {
+                List<string> list1 = new List<string>(1000000);
+                List<string> list2 = new List<string>(1000000);
+                List<string> list3 = new List<string>(1000000);
+                public void Add()
+                {
+                    Console.WriteLine("请输入商品编号" + num);
+                    num = Console.ReadLine();
+                    Console.WriteLine("请输入商品名称" + name);
+                    name = Console.ReadLine();
+                    Console.WriteLine("请输入您的姓名" + people);
+                    people = Console.ReadLine();
+                    list1.Add(num);
+                    list2.Add(name);
+                    list3.Add(people);
+                }
+                public void Delete()
+                {
+                    Console.WriteLine("请输入姓名进行删除：");
+                    string choose = Console.ReadLine();
+                    int i ;
+                    if(list3.Contains(choose))
+                    {
+                        for (i = 0; i < 1000000; i++)
+                        {
+                            if (list3[i] == choose)
+                                break;
+                        }
+                        list1.RemoveAt(i);
+                        list2.RemoveAt(i);
+                        list3.RemoveAt(i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("没有该姓名！");
+                    }
+                }
+                public void Scan()
+                {
+                    Console.WriteLine("请输入姓名进行浏览：");
+                    string choose = Console.ReadLine();
+                    int i;
+                    for (i = 0; i < 1000000; i++)
+                    {
+                        if (list3[i] == choose)
+                            break;
+                    }
+                    Console.WriteLine(list1[i] + "   " + list2[i] + "   " + list3[i]);
+                }
+                bool p = true;
+                public void Break()
+                {
+                    p = false;
+                }
+                static void Main(string[] args)
+                {
+                    OrderDetails or = new OrderDetails();
+                    while(or.p)
+                    {
+                        Console.WriteLine("请输入要进行的操作：");
+                        string action = Console.ReadLine();
+                        switch (action)
+                        {
+                            case "add":
+                                or.Add();
+                                break;
+                            case "delete":
+                                or.Delete();
+                                break;
+                            case "scan":
+                                or.Scan();
+                                break;
+                            case "break":
+                                or.Break();
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
