71e78736609c16bcdc2a8e87539026eb16625f7d
wdzp <43175481+wdzp@users.noreply.github.com>
2018-10-24 20:21:21 +0800

    HomeWork6

---
+++ HomeWork6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+namespace TestSerialize
+{
+    public class Order
+    {
+        public string num{ get; set; }
+        public string name{ get; set; }
+        public string people{ get; set; }
+    }
+    public class OrderService
+    {
+        public void Export()
+        {
+            Order order = new Order();
+            order.num = Console.ReadLine();
+            order.name = Console.ReadLine();
+            order.people = Console.ReadLine();
+            var xDoc = new XDocument(new XElement("root",
+                new XElement("编号",
+                new XText(order.num),
+                new XAttribute("名称", order.name)),
+                new XAttribute("姓名", order.people)));
+            StreamWriter sw = new StreamWriter(new FileStream(@"d:\t.xml", FileMode.Create),
+                Encoding.UTF8);
+            xDoc.Save(sw);
+            xDoc.Save(Console.Out);
+        }
+        public void Import()
+        {
+            var xDoc = new XDocument(new XElement("root",
+                new XElement("dog",
+                new XText("小狗"),
+                new XAttribute("color", "black")),
+            new XElement("cat"),
+            new XElement("pig", "小猪")));
+            StreamWriter sw = new StreamWriter(new FileStream(@"d:\t.xml", FileMode.Create),
+                Encoding.UTF8);
+            xDoc.Save(sw);
+            var query = from item in xDoc.Element("root").Elements()
+                select new
+                {
+                    TypeName = item.Name,
+                    Saying = item.Value,
+                    Color = item.Attribute("color") == null
+                    ? null : item.Attribute("color").Value
+                };
+            foreach(var item in query)
+            {
+                Console.WriteLine("{0}'s color is {1},{0} said {2}",
+                    item.TypeName,
+                    item.Color??"Unkown",
+                    item.Saying??"nothing");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService order = new OrderService();
+            var a = Console.ReadLine();
+            switch(a)
+            {
+                case "export":
+                    order.Export();
+                    break;
+                case "import":
+                    order.Import();
+                    break;
+            }
+        }
+    }
+}
---
+++ HomeWork6/ConsoleApp1/ConsoleApp1Tests1/OrderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestSerialize;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSerialize.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void ImportTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
