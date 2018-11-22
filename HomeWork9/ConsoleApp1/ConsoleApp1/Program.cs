d05daef13977929c68c17fa22713f3c8d78bfccd
wdzp <43175481+wdzp@users.noreply.github.com>
2018-11-22 21:56:00 +0800

    HomeWork9

---
+++ HomeWork9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+public class Crawler
+{
+    private Hashtable urls = new Hashtable();
+    private int count = 0;
+    static void Main(string[]args)
+    {
+        Crawler myCrawler = new Crawler();
+        string startUrl = "http://www.cnblogs.com/dstang2000/";
+        if (args.Length >= 1) startUrl = args[0];
+        myCrawler.urls.Add(startUrl, false);
+        new Thread(myCrawler.Crawl).Start();
+    }
+
+    private void Crawl()
+    {
+        Console.WriteLine("开始爬行了");
+        while(true)
+        {
+            string current = null;
+            foreach(string url in urls.Keys)
+            {
+                if ((bool)urls[url]) continue;
+                current = url;
+            }
+            if (current == null || count > 10) break;
+            Console.WriteLine("爬行" + current + "页面");
+            Task<string> html = DownLoadAsync(current);
+            urls[current] = true;
+            count++;
+            Parse(html.Result);
+        }
+        Console.WriteLine("爬行结束");
+    }
+    public async Task<string> DownLoadAsync(string url)
+    {
+        try
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            string html =await webClient.DownloadStringTaskAsync(new Uri(url));
+            string filename = count.ToString();
+            File.WriteAllText(filename, html, Encoding.UTF8);
+            return html;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return "";
+        }
+    }
+    public void Parse(string html)
+    {
+        string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+        MatchCollection matches = new Regex(strRef).Matches(html);
+        foreach(Match match in matches)
+        {
+            strRef=match.Value.Substring(match.Value.IndexOf('=')+1).Trim('"','\'','#',' ','>');
+            if (strRef.Length == 0) continue;
+            if (urls[strRef] == null) urls[strRef] = false;
+        }
+    }
+}
